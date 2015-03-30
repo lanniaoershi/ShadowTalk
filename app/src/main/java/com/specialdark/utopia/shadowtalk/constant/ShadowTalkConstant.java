40c3fdba0a49e120e60edcb10c3990a678f0f4d2
utopia <lanniaoershi@gmail.com>
2015-03-30 21:14:56 +0800

    Add constant class to manager constant

---
+++ app/src/main/java/com/specialdark/utopia/shadowtalk/constant/ShadowTalkConstant.java
@@ -0,0 +1,11 @@
+package com.specialdark.utopia.shadowtalk.constant;
+
+/**
+ * Created by wei on 2015/3/30/0030.
+ */
+public class ShadowTalkConstant {
+
+    public static final String ACTION_BLUETOOTH_CHAT_ACTIVITY = "com.specialdark.utopia.shadowtalk.BLUETOOTH_CHAT";
+
+
+}
